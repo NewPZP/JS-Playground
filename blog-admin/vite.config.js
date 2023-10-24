d9a3aa3687939d45b0ee223ecfec4fa4d04f0e62
Pan <523141861@qq.com>
2023-10-25 07:37:56 +0800

    update

--- blog-admin/vite.config.js
+++ blog-admin/vite.config.js
@@ -1,15 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    hmr: true,
-    port: 3001,
-    open: true
-  },
+  // server: {
+  //   hmr: true,
+  //   port: 3001,
+  //   open: true
+  // },
+  mockServer: ({
+    logLevel: 'info',
+    middlewares: [
+      cookieParser(),
+      bodyParser.json(),
+      bodyParser.urlencoded(),
+      bodyParser.text(),
+      bodyParser.raw()
+    ]
+  }),
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
