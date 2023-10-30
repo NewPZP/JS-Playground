b845214864d5235b658672874dbe25495b6acebb
Pan <523141861@qq.com>
2023-10-31 07:19:26 +0800

    update

--- blog-admin/src/views/Framework.vue
+++ blog-admin/src/views/Framework.vue
@@ -1,13 +1,107 @@
 <template>
-    <div>framework
+    <div class="layout">
+      <el-container >
+        <el-header class="header">Header</el-header>
+        <el-container class="container">
+          <el-aside  width='200px' class="left-aside">
+            <div>
+              <el-button class="post-btn">发布</el-button>
+            </div>
+          </el-aside>
+          <el-main class="right-main">Main</el-main>
+        </el-container>
+      </el-container>
     </div>
   </template>
   
-  <script setup>
-  </script>
+<script setup>
+import { ref } from 'vue'
+const menuList = ref([
+{
+  title: '博客',
+  icon: 'icon-content',
+  open: true,
+  children: [
+    {
+      title: '博客管理', 
+      path: '/blog/list'
+    },
+    {
+      title: '分类管理',
+      path: '/blog/category'
+    },
+
+  ],
+},
+{
+  title: '专题',
+  icon: 'icon-topic',
+  open: true,
+  children: [
+  {
+    title: '专题管理',
+    path: '/topic/list'}
+  ]
+},
+{
+  title: '设置',
+  icon: 'icon-setting',
+  open: true,
+  children: [
+    {
+      title: '个人信息设置',
+      path: '/setting/my'
+    },
+    {
+      title: '博客成员',
+      path: '/setting/member'
+    },
+    {
+      title: '系统设置',
+      path: '/setting/system'
+    },
+  ]
+},
+{
+  title: '回收站',
+  icon: 'icon-recycle',
+  open: true,
+  children: [
+    {
+      title: '回收站',
+      path: '/recycle/list'
+    }
+  ]
+}
+])
+
+</script>
+
   
-  
-  
-  <style scoped>
-  </style>
+<style lang="scss">
+.layout {
+  .header{
+    border-bottom: 1px solid #ddd;
+  }
+  .container{ 
+    padding-top: 10px;
+    background: #f0f6f7;
+    height: calc(100vh - 60px);
+
+    .left-aside{
+      padding: 0px 15px;
+      width: 250px;
+      .post-btn{
+         background-color: orange;
+         color: #fff;
+         height: 40px;
+         width: 100%;
+      }
+    }
+    .right-main{
+      background: #fff;
+    }
+  }
+}
+</style>
   
